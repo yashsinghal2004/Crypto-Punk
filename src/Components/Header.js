c123c520970a61a611b95cb936de697a48666391
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-02-04 13:22:46 +0000

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,23 @@
 import logo from "../images/header/cryptopunk-logo.png";
+import search from "../images/header/search.png";
 const Header = () => {
   return (
-    <div className="p-5">
+    <div className="p-5 flex justify-between ">
       <div>
-        <img className="w-48" src={logo} alt="logo" />
+        <img className="w-44 " src={logo} alt="logo" />
+      </div>
+      <div className=" bg-[#1c1c1e] m-3 h-10 align-center flex flex-1 rounded-full pl-3">
+        <img src={search} alt="Search icon" className="p-2" />
+        <input
+          type="text"
+          placeholder="Collection, item or user"
+          className="bg-transparent p-1 text-white outline-none w-full text-lg"
+        />
+      </div>
+      <div className="flex  text-[#a1a5b0] ">
+        <p className="m-4">Drops</p>
+        <p className="m-4">Marketplace</p>
+        <p className="m-4">Create</p>
       </div>
     </div>
   );
